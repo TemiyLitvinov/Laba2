8a701f36a1ab20b90faaffb1a9f5fd75bae8eaa1
Artemiy <artoym13571@gmail.com>
2025-11-22 14:33:44 +0300

    Добавлены комментарии

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from check_password import is_valid_password, password_regex
 
+# Главная функция программы
 def main():
+    # Бесконечный цикл
     while True:
         print("\nВыберите действие")
         print("1 — Проверить введённый пароль")
@@ -8,41 +10,55 @@ def main():
         print("3 — Выход")
 
         choice = input().strip()
+
+        # Блок с проверкой надёжности введённого пароля
         if choice == "1":
             password = input("Введите пароль: ")
+
+            # Проверка надёжности пароля с помощью функции, написанной в файле check_password
             if is_valid_password(password):
                 print("Пароль является надёжным")
             else:
                 print("Пароль не соответствует критериям надёжности")
 
+        # Блок с считыванием данных из файла и проверкой надёжности паролей из него
         elif choice == "2":
             filename = input("Введите путь к файлу или его имя: ")
+
+            # Блок try/except для открытия файла и ловли исключения при ошибке
             try:
                 with open(filename, "r", encoding="utf-8", errors="ignore") as file:
                     lines = file.read().splitlines()
 
+                # Список найденных надёжных паролей
                 found_good_passwords = [line for line in lines if password_regex.match(line)]
 
                 if found_good_passwords:
                     print('\nНайденные надежные пароли:')
+
+                    # Вывод всех паролей с нумерацией
                     for i in range(len(found_good_passwords)):
                         print(f'{i + 1}. {found_good_passwords[i]}\n')
 
                 else:
                     print(f'В файле нет надёжных паролей')
 
+            # Исключение для случая, если файл окажется не найден
             except FileNotFoundError:
                 print(f"Ошибка: файл '{filename}' не найден!")
                 return ""
 
+        # Выход из бесконечного цикла
         elif choice == "3":
             print("Выход...")
             break
 
+        # Обработка для случая ввода других символов
         else:
             print("Некорректный ввод. Попробуйте снова.")
             return ""
 
 
+# Запуск главной функции
 if __name__ == "__main__":
     main()
--- unit_test.py
+++ unit_test.py
@@ -1,31 +1,41 @@
 import unittest
 from check_password import is_valid_password
 
+# Класс, в котором реализованы Unit-тесты, наследуемый от класса "unittest.TestCase"
 class TestPasswordRegex(unittest.TestCase):
 
+    # Unit-тест для пароля, проходящего по всем критериям
     def test_valid(self):
-        self.assertTrue(is_valid_password("OKPassword321$"))
+        self.assertTrue(is_valid_password("NormPass321$"))
 
+    # Unit-тест для пароля, в котором не хватает заглавной буквы
     def test_no_uppercase(self):
         self.assertFalse(is_valid_password("badpassword5729*"))
 
+    # Unit-тест для пароля, в котором не хватает строчных букв
     def test_no_lowercase(self):
         self.assertFalse(is_valid_password("BIGPASSWORD*7843"))
 
+    # Unit-тест для пароля, в котором не хватает строчных цифр
     def test_no_digit(self):
         self.assertFalse(is_valid_password("NoDigitsPassword$"))
 
+    # Unit-тест для пароля, в котором не хватает специальных символов
     def test_no_special(self):
         self.assertFalse(is_valid_password("CommonPassword#"))
 
+    # Unit-тест для пароля, длина которого < 12
     def test_short(self):
         self.assertFalse(is_valid_password("tooShort2&"))
 
+    # Unit-тест для пароля, в котором присутствуют пробелы
     def test_with_space(self):
         self.assertFalse(is_valid_password("Hello world!341"))
 
+    # Unit-тест для длинного и проходящего по всем критериям пароля
     def test_long_valid(self):
         self.assertTrue(is_valid_password("GoodPassword3748@"))
 
+# Запуск всех тестов
 if __name__ == "__main__":
     unittest.main()
